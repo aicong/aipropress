073f83f00b1290ca5a7f726daa4046876ed6fdb5
aicong <383640116@qq.com>
2024-11-15 14:01:07 +0800

    first commit

---
+++ assets/develop_index.md.BH3uL5Ii.lean.js
@@ -0,0 +1,13 @@
+import{_ as s,c as n,a0 as p,o as e}from"./chunks/framework.D4Ro5fhV.js";const h=JSON.parse('{"title":"目录结构","description":"","frontmatter":{},"headers":[],"relativePath":"develop/index.md","filePath":"develop/index.md"}'),l={name:"develop/index.md"};function i(t,a,c,o,d,r){return e(),n("div",null,a[0]||(a[0]=[p(`<h1 id="目录结构" tabindex="-1">目录结构 <a class="header-anchor" href="#目录结构" aria-label="Permalink to &quot;目录结构&quot;">​</a></h1><br><br><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>-- packages</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>  -- cli 脚手架(create-apui2)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>  -- components 本地开发组件库(@apui2/components)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>  -- apui2 打包后组件库,既要发布的组件库(apui2)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>  -- utils 工具包(@apui2/utils)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>-- play 测试开发组件库的Vue3项目目录</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>-- site 组件库文档站点目录</span></span></code></pre></div>`,4)]))}const m=s(l,[["render",i]]);export{h as __pageData,m as default};
